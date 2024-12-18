1d7728f0f6a6cf147ba1873cc2a0d90968224acb
Andrew Nguyen <andrewnguyen.x@gmail.com>
2024-12-18 11:47:10 +1030

    removed old

--- old/assets/mobile/index.js
+++
@@ -1,6 +0,0 @@
-var map = L.map('map', { zoomControl: false }).setView([-37.8124, 144.9623], 11);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
